f9a2476ab12e8ed4003e47d37204fc22a8d0c1c4
frohlichramon01 <frohlichramon01@gmail.com>
2025-01-15 20:04:13 +0000

    aula3

---
+++ src/components/Banner/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+// import { Cores } from '../../styles'
+
+export const Imagem = styled.div`
+  width: 100%;
+  height: 560px;
+  display: block;
+  background-size: cover;
+  background-repeat: no-repeat;
+  font-weight: bold;
+  padding-top: 340px;
+`
+export const Titulo = styled.h2`
+  font-size: 36px;
+  max-width: 450px;
+`
+export const Precos = styled.p`
+  font-size: 24px;
+  margin-top: 24px;
+
+  span {
+    text-decoration: line-through;
+  }
+`
--- src/styles.ts
+++ src/styles.ts
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components'
 
 export const Cores = {
   branca: '#EEEEEE',
@@ -21,9 +21,10 @@ export const GlobalCss = createGlobalStyle`
     color: ${Cores.branca};
     padding-top: 40px;
   }
-`
-export const Container = styled.div`
-  max-width: 1024px;
-  width: 100%;
-  margin: 0 auto;
+
+  .container{
+    max-width: 1024px;
+    width: 100%;
+    margin: 0 auto;
+  }
 `
