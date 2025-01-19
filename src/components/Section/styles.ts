4e6e6d10eb400df96114a789e4a8430d5cff30cb
frohlichramon01 <frohlichramon01@gmail.com>
2025-01-19 16:39:38 +0000

    mod 36 aula 3

--- src/components/Section/styles.ts
+++ src/components/Section/styles.ts
@@ -13,14 +13,14 @@ export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
     background-color: ${(props) =>
       props.background === 'black' ? Cores.cinza : Cores.preta};
   }
+
+  p {
+    font-size: 14px;
+    line-height: 22px;
+    max-width: 640px;
+  }
 `
 
-export const List = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  column-gap: 24px;
-  margin-top: 40px;
-`
 export const Title = styled.h2`
   font-size: 18px;
   font-weight: bold;
--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,5 +1,6 @@
 import { useParams } from 'react-router-dom'
 import Hero from '../../components/Hero'
+import Section from '../../components/Section'
 
 const Product = () => {
   const { id } = useParams()
@@ -7,6 +8,27 @@ const Product = () => {
   return (
     <>
       <Hero />
+      <Section title="Sobre o jogo" background="black">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure corporis
+          nesciunt perferendis quas aliquam nemo facilis qui debitis, rerum enim
+          quis quam, porro reprehenderit minima alias nulla consectetur
+          assumenda? Facere.
+        </p>
+      </Section>
+      <Section title="Mais detalhes" background="gray">
+        <p>
+          <b>Plataforma:</b> Playstation 5 <br />
+          <b>Desenvolvedor:</b> Avalanche Software <br />
+          <b>Editora:</b> Portkey Games, subsidiária da Warner Bros. Interactive
+          Entertainment
+          <br />
+          <b>Idiomas:</b> O jogo oferece suporte a diversos idiomas...
+        </p>
+      </Section>
+      <Section title="Galeria" background="black">
+        <div>Fotos</div>
+      </Section>
     </>
   )
 }
